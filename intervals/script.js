6c2393e08abb4e7c47847ac3c70cb9044c81cf42
Marvolo <aidan.sainte@gmail.com>
2023-08-03 16:34:48 -0400

    first commit

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,9 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+function incrementCounter() {
+    time++; 
+    counterElement.innerText = time;
+} 
+
+setInterval(incrementCounter, 1000);
--- race-to-the-finish/sketch.js
+++ race-to-the-finish/sketch.js
@@ -1,7 +1,7 @@
 let redXPos = 25;
-let redYPos = 100;
+let redYPos = 225;
 let blueXPos = 25;
-let blueYPos = 200;
+let blueYPos = 25;
 let redScore = 0;
 let blueScore = 0;
 
@@ -17,41 +17,44 @@ function draw() {
 
     // move both balls random distances
     redXPos += random(-1, 5);
-    redYPos += random(-0.5, 0.5);
+    // redYPos += random(-0.5, 0.5);
     blueXPos += random(-1, 5);
-    blueYPos += random(-0.5, 0.5);
+    // blueYPos += random(-0.5, 0.5);
 
     // show scores
     textSize(60);
-    fill(255, 0, 0);
-    text(redScore, 800, 100);
     fill(0, 0, 255);
-    text(blueScore, 800, 225);
+    text(blueScore, 700, 100);
+    fill(255, 0, 0);
+    text(redScore, 700, 225);
 
     // show finish line
-    line(600, 0 600, 300);
+    line(600, 0, 600, 300);
 
     // red ball
     fill(255, 0, 0);
-    ellipse(50, 50, redXPos, redYPos);
+    ellipse(redXPos, redYPos, 50, 50);
+    
+
     
     // blue ball
     fill(0, 0, 255);
-    ellipse(50, 50, blueXPos, blueYPos);
+    ellipse(blueXPos, blueYPos, 50, 50);
+    
 
     // crossed finish line
-    if (redXPos < 575 || blueXPos < 575) {
+    if (redXPos > 575 || blueXPos > 575) {
         if (redXPos > blueXPos) {
             // blue wins
             redScore += 1;
         } else if (blueXPos > redXPos) {
             // red wins
-            blueScore = 1;
+            blueScore += 1;
         }
         // reset positions
         redXPos = 25;
-        redYPos = 100;
+        redYPos = 225;
         blueXPos = 25;
-        blueYPos = 200;
+        blueYPos = 25;
     }
 }
